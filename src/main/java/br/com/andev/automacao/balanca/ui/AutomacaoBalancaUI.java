20c32786fda8ae24a335b0f417c9b203e86e4d15
Augusto Neto <augusto.soliveiran@gmail.com>
2016-07-15 00:45:08 -0300

    Updating project

--- src/main/java/br/com/andev/automacao/balanca/ui/AutomacaoBalancaUI.java
+++ src/main/java/br/com/andev/automacao/balanca/ui/AutomacaoBalancaUI.java
@@ -1,11 +1,11 @@
 package br.com.andev.automacao.balanca.ui;
 
 import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
@@ -14,28 +14,60 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
+import br.com.andev.automacao.balanca.SerialComm;
 import br.com.andev.automacao.balanca.dao.PedidoPesoDao;
 import br.com.andev.automacao.balanca.dao.PedidoPesoDaoJpa;
 import br.com.andev.automacao.balanca.dao.PersistenceManager;
 import br.com.andev.automacao.balanca.model.PedidoPeso;
 
-public class AutomacaoBalancaUI {
+public class AutomacaoBalancaUI implements Runnable {
 
 	private JFrame janela;
 
 	private JPanel painelPrincipal;
 	
 	private JLabel jlNumPedido, jlNumNotaFiscal, jlOperador, jlPeso;
-	private JTextField jtfNumPedido, jtfNumNotaFiscal, jtfOperador, jtfPeso;
+	private JTextField jtfNumPedido, jtfNumNotaFiscal, jtfOperador;
+	private JTextField jtfPeso;
 	
 	private JButton botaoGravar, botaoSair;
 	
 	private PedidoPesoDao pedidoPesoDao;
 	
+	private InputStream in;
+	private String dadoLido = new String();
+	
+	public String getDadoLido() {
+		return dadoLido;
+	}
+	
+	public void setDadoLido(String dadoLido) {
+		this.dadoLido = dadoLido;
+	}
 
 	public static void main(String[] args) {
-		new AutomacaoBalancaUI().montaTela();
+		try {
+			
+			SerialComm serialComm = new SerialComm();
+			serialComm.connect("/dev/ttyUSB02");
+			
+			(new Thread(new AutomacaoBalancaUI(serialComm.getInputStream()))).start();
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public AutomacaoBalancaUI(InputStream in) {
+		this.in = in;
+		montaTela();
 	}
 
 	public void montaTela() {
@@ -85,7 +117,7 @@ public class AutomacaoBalancaUI {
 		jlPeso = new JLabel("Peso");
 		adiciona(jlPeso, 10, 91, 130, 25);
 
-		jtfPeso = new JTextField(20);
+		jtfPeso = new JTextField("9999999", 20);
 		adiciona(jtfPeso, 130, 91, 300, 25);
 	}
 
@@ -134,6 +166,35 @@ public class AutomacaoBalancaUI {
 	private void adiciona(Component component, int nColuna, int nLinha, int nLargura, int nAltura) {
 		painelPrincipal.add(component);
 		component.setBounds(nColuna, nLinha, nLargura, nAltura);
+	}
+	
+	public void run() {
+        byte[] buffer = new byte[1024];
+        int len = -1;
+        try {
+            while ((len = this.in.read(buffer)) > -1 ) {
+                //System.out.print(new String(buffer,0,len));
+                setDadoLido(new String(buffer,0,len));
+                System.out.print(getDadoLido());
+                atualizaPeso(getDadoLido());
+        
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }  
+        
+    }
+	
+	public void atualizaPeso(final String peso) {
+		Runnable atualizaPeso = new Runnable() {
+		     public void run() {
+		    	 System.out.println("Atualizando Peso: " + peso);
+		    	 jtfPeso.setText(peso);
+		     }
+		 };
+
+		 SwingUtilities.invokeLater(atualizaPeso);
 	}
 
 }
